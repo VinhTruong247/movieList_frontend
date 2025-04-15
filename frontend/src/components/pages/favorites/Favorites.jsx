6b53d9c18ca9a7fe6eb4d734c93dc20460761ed6
Vinh Truong <roberttruong247@gmail.com>
2025-04-15 18:13:58 +0700

    Restyling favorite list in Profile Page

--- frontend/src/components/pages/favorites/Favorites.jsx
+++ frontend/src/components/pages/favorites/Favorites.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import MovieCard from '../home/movieCard/MovieCard';
+import { useNavigate } from 'react-router-dom';
 import { useFavorites } from '../../../hooks/useFavorites';
-import { Link, useNavigate } from 'react-router-dom';
+import FavoriteComponent from './FavoriteComponent';
 import './Favorites.scss';
 
 const Favorites = () => {
@@ -30,24 +30,8 @@ const Favorites = () => {
         </svg>
       </button>
       <h1 className="page-title">Your Favorite Movies</h1>
-
-      {favorites.length === 0 ? (
-        <div className="empty-state">
-          <h3>No favorite movies yet</h3>
-          <p>Start adding movies to your favorites to see them here!</p>
-          <Link to="/" className="browse-button">
-            Browse Movies
-          </Link>
-        </div>
-      ) : (
-        <div className="favorites-grid">
-          {favorites.map((movie) => (
-            <div key={movie.id} className="favorite-item">
-              <MovieCard movie={movie} />
-            </div>
-          ))}
-        </div>
-      )}
+      
+      <FavoriteComponent favorites={favorites} />
     </div>
   );
 };
