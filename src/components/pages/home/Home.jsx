ded43fcebd881d32d1c7545a56a53e027cedd625
Vinh Truong <roberttruong247@gmail.com>
2025-04-25 17:06:34 +0700

    Fix default filter state

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import MovieCarousel from "./movieCarousel/MovieCarousel";
 import MovieCard from "./movieCard/MovieCard";
 import GenreList from "./movieGenreList/GenreList";
@@ -10,7 +10,6 @@ const Home = () => {
   const { movies, loading, error } = useMovies();
   const [filteredMovies, setFilteredMovies] = useState([]);
   const [selectedGenre, setSelectedGenre] = useState("all");
-  const [activeFilter, setActiveFilter] = useState("all");
   const [searchQuery, setSearchQuery] = useState("");
   const [activeMovieType, setActiveMovieType] = useState("all");
   const [activeSortType, setActiveSortType] = useState("all");
@@ -18,40 +17,6 @@ const Home = () => {
   const handleGenreSelect = (genre) => {
     setSelectedGenre(genre);
     filterAndSortMovies(activeMovieType, activeSortType, genre);
-  };
-
-  const handleFilter = (filter) => {
-    setActiveFilter(filter);
-
-    let sortedMovies = [...movies];
-
-    if (searchQuery) {
-      sortedMovies = sortedMovies.filter((movie) =>
-        movie.title.toLowerCase().includes(searchQuery.toLowerCase())
-      );
-    }
-
-    if (selectedGenre !== "all") {
-      sortedMovies = sortedMovies.filter((movie) =>
-        movie.genre.includes(selectedGenre)
-      );
-    }
-
-    if (filter === "Movie") {
-      sortedMovies = sortedMovies.filter((movie) => movie.type === "Movie");
-    } else if (filter === "TV Series") {
-      sortedMovies = sortedMovies.filter((movie) => movie.type === "TV Series");
-    }
-    if (filter === "all") {
-      sortedMovies.sort((a, b) => a.id - b.id);
-    } else if (filter === "top-rated") {
-      sortedMovies.sort((a, b) => b.imdb_rating - a.imdb_rating);
-    } else if (filter === "latest") {
-      sortedMovies.sort((a, b) => b.year - a.year);
-    } else {
-      sortedMovies.sort((a, b) => a.id - b.id);
-    }
-    setFilteredMovies(sortedMovies);
   };
 
   const handleSearch = (event) => {
@@ -169,7 +134,7 @@ const Home = () => {
         </aside>
 
         <div className="main-content">
-          {filteredMovies.length > 0 ? (
+          {filteredMovies.length < 0 ? (
             <div className="movies-grid">
               {filteredMovies.map((movie) => (
                 <MovieCard key={movie.id} movie={movie} />
