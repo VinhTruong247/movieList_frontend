69c2d91db4857ce9278c44b6a1ae67e5d14c969e
Vinh Truong <roberttruong247@gmail.com>
2025-05-21 11:39:05 +0700

    Fix undefined prop in FavoriteComponent

--- src/components/pages/favorites/FavoriteComponent.jsx
+++ src/components/pages/favorites/FavoriteComponent.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router";
 import MovieCard from "../home/movieCard/MovieCard";
 
-const FavoriteComponent = ({ syncedFavorites }) => {
+const FavoriteComponent = ({ syncedFavorites = [] }) => {
   return (
     <>
-      {syncedFavorites.length === 0 ? (
+      {!syncedFavorites || syncedFavorites.length === 0 ? (
         <div className="empty-state">
           <h3>No favorite movies yet</h3>
           <p>Start adding movies to your favorites to see them here!</p>
