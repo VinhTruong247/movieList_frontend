35f98fa20e7bb737a85f3ba2c0ff63afbe77d2f3
Vinh Truong <roberttruong247@gmail.com>
2025-06-23 10:54:52 +0700

    Complete migrate Redux replacing MovieProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import supabase from "./supabase-client";
+import { useDispatch } from "react-redux";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, useRoutes } from "react-router";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { MovieProvider } from "./context/MovieContext";
+import { fetchCurrentUser } from "./redux/slices/authSlice";
 import AppRoutes from "./router/AppRoutes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./main.scss";
+
+const AuthStateHandler = ({ children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCurrentUser());
+
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      async (event) => {
+        if (event === "SIGNED_IN") {
+          setTimeout(() => {
+            dispatch(fetchCurrentUser());
+          }, 300);
+        }
+      }
+    );
+
+    return () => {
+      if (authListener?.subscription?.unsubscribe) {
+        authListener.subscription.unsubscribe();
+      }
+    };
+  }, [dispatch]);
+
+  return children;
+};
 
 const AppRouter = () => {
   const routes = useRoutes(AppRoutes);
@@ -17,12 +45,12 @@ const AppRouter = () => {
 function App() {
   return (
     <Provider store={store}>
-      <MovieProvider>
-        <Router>
+      <Router>
+        <AuthStateHandler>
           <ToastContainer position="top-right" autoClose={3000} />
           <AppRouter />
-        </Router>
-      </MovieProvider>
+        </AuthStateHandler>
+      </Router>
     </Provider>
   );
 }
